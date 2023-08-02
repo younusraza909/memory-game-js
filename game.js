8a3a044483e94884289784a23ffedfbd76363293
younusraza909 <younusraza909@gmail.com>
2023-08-02 16:35:49 +0500

    rendering Game logic

--- game.js
+++ game.js
@@ -1,63 +1,41 @@
-const cards = document.querySelectorAll(".memory-card");
+// Select the game container element
+const gameContainer = document.querySelector(".game-container");
 
-let hasFlippedCard = false;
-let lockBoard = false;
-let firstCard, secondCard;
+// Function to create a card element
+function createCard() {
+  const card = document.createElement("div");
+  card.classList.add("card");
 
-function flipCard() {
-  if (lockBoard) return;
-  if (this === firstCard) return;
+  const innerCard = document.createElement("div");
+  innerCard.classList.add("card-inner");
 
-  this.classList.add("flip");
+  const frontCard = document.createElement("div");
+  frontCard.classList.add("card-front");
 
-  if (!hasFlippedCard) {
-    // first click
-    hasFlippedCard = true;
-    firstCard = this;
+  const backCard = document.createElement("div");
+  backCard.classList.add("card-back");
 
-    return;
-  }
+  innerCard.appendChild(frontCard);
+  innerCard.appendChild(backCard);
+  card.appendChild(innerCard);
 
-  // second click
-  secondCard = this;
-
-  checkForMatch();
-}
-
-function checkForMatch() {
-  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-
-  isMatch ? disableCards() : unflipCards();
-}
-
-function disableCards() {
-  firstCard.removeEventListener("click", flipCard);
-  secondCard.removeEventListener("click", flipCard);
-
-  resetBoard();
-}
-
-function unflipCards() {
-  lockBoard = true;
-
-  setTimeout(() => {
-    firstCard.classList.remove("flip");
-    secondCard.classList.remove("flip");
-
-    resetBoard();
-  }, 1500);
-}
-
-function resetBoard() {
-  [hasFlippedCard, lockBoard] = [false, false];
-  [firstCard, secondCard] = [null, null];
-}
-
-(function shuffle() {
-  cards.forEach((card) => {
-    let randomPos = Math.floor(Math.random() * 12);
-    card.style.order = randomPos;
+  // Toggle the 'click' class on card click
+  card.addEventListener("click", () => {
+    card.classList.toggle("click");
   });
-})();
 
-cards.forEach((card) => card.addEventListener("click", flipCard));
+  return card;
+}
+
+// Function to render the game grid
+function renderGame() {
+  for (let i = 0; i < 6; i++) {
+    for (let y = 0; y < 4; y++) {
+      const card = createCard(); // Create a card
+      gameContainer.appendChild(card); // Append card to the game container
+    }
+  }
+}
+
+// Wait for the DOM to be fully loaded before rendering the game
+document.addEventListener("DOMContentLoaded", renderGame);
